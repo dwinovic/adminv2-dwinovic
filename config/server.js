823cababb9da16e976aef16b3ef7c8133fc323e8
dwinovic <nd.cahya19@gmail.com>
2021-05-05 22:03:29 +0700

    Update database config

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eb4699fcae8c4a41e19605555c992d03'),
+    },
+  },
+});
