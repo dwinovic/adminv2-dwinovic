1ada768e1a5974fb9bdccab9913cdc480512b9ee
dwinovic <nd.cahya19@gmail.com>
2021-05-05 22:39:56 +0700

    updated database

--- config/database.js
+++ config/database.js
@@ -1,37 +1,19 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         srv: env.bool('DATABASE_SRV', false),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'dwinovicDB'),
-//         username: env('DATABASE_USERNAME', null),
-//         password: env('DATABASE_PASSWORD', null),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//     },
-//   },
-// });
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
-        srv: env.bool("DATABASE_SRV", true),
+        host: env("DATABASE_HOST", "cluster0.vy5pd.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", false),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME", "dwinovicDB"),
+        username: env("DATABASE_USERNAME", null),
+        password: env("DATABASE_PASSWORD", null),
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: true,
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
